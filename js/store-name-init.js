c81da0671ae8afb893d73a1471b419ee6b0b82aa
unknown <c830627@fme.com.tw>
2026-07-09 14:35:30 +0800

    feat: set default store name to 日翊員購小舖

--- js/store-name-init.js
+++ js/store-name-init.js
@@ -1,5 +1,5 @@
 ﻿(function(){
-  var n = localStorage.getItem('reyi_ep_store_name');
+  var n = localStorage.getItem('reyi_ep_store_name') || '日翊員購小舖';
   if (!n) return;
   function apply() {
     document.querySelectorAll('.store-name-el').forEach(function(el){ el.textContent = n; });
